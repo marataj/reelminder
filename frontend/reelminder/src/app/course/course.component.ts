e26aa12685adb7f542bae775f5eb66d6a4a4d2d3
Mati <ratajczak.mat@gmail.com>
2024-01-02 23:45:19 +0100

    Add course component with basic YT player embedded

--- frontend/reelminder/src/app/course/course.component.ts
+++ frontend/reelminder/src/app/course/course.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
+  public video: any;
+  public player: any;
+
+  ngOnInit() {
+    this.video = 'dTLAWHWvtew';
+    this.startPlayer();
+  }
+
+  startPlayer() {
+    var tag = document.createElement('script');
+    tag.src = 'http://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    window['onYouTubeIframeAPIReady'] = () => this.initPlayer();
+  }
+
+  initPlayer() {
+    this.player = new window['YT'].Player('player', {
+      videoId: this.video,
+      playerVars: {
+        autoplay: 1,
+        modestbranding: 1,
+        controls: 1,
+        disablekb: 1,
+        rel: 0,
+        showinfo: 0,
+        fs: 0,
+        playsinline: 1
+      },
+      events: {
+        'onStateChange': this.onPlayerStateChange.bind(this),
+        'onReady': this.onPlayerReady.bind(this),
+      }
+    });
+  }
+
+  onPlayerReady(event) {
+  }
+
+  onPlayerStateChange(event) {
+  }
+      
+  cleanTime() {
+    return Math.round(this.player.getCurrentTime())
+  }
 
 }
