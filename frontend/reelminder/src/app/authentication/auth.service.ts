14f08e3c7548e18d8b35f6bf0bec609182e0cef9
Mati <ratajczak.mat@gmail.com>
2024-03-09 21:21:28 +0100

    Add logout handling on the frontend

--- frontend/reelminder/src/app/authentication/auth.service.ts
+++ frontend/reelminder/src/app/authentication/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { AuthResponseData, LoginData, RegisterData, User } from './auth.model';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,18 @@ export class AuthService {
   user = new BehaviorSubject<User>(null);
   readonly AUTH_UTL: string = 'http://127.0.0.1:8000/auth/';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   register(user: RegisterData): Observable<any> {
     return this.httpClient.post(this.AUTH_UTL + 'register/', user);
   }
   login(user: LoginData): Observable<any> {
     return this.httpClient.post(this.AUTH_UTL + 'login/', user);
+  }
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(['']);
   }
 
   handleAuthentication(authResponse: AuthResponseData) {
--- frontend/reelminder/src/app/navbar/navbar.component.ts
+++ frontend/reelminder/src/app/navbar/navbar.component.ts
@@ -20,4 +20,8 @@ export class NavbarComponent implements OnInit {
       this.username = user.username;
     });
   }
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
