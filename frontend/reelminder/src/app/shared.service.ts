1deafc081aa552967f8de901a33dff65ec638d0d
marataj <user.name>
2024-06-12 23:27:31 +0200

    Add docker related files, adjust app for deployment

--- frontend/reelminder/src/app/shared.service.ts
+++ frontend/reelminder/src/app/shared.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class SharedService {
   // TODO: add type of course and note
   constructor(private httpClient: HttpClient) {}
-  readonly APIUrl = 'http://127.0.0.1:8000';
+  readonly APIUrl = environment.API_URL;
 
   getCourseList(): Observable<any[]> {
     return this.httpClient.get<any[]>(this.APIUrl + '/course/');
