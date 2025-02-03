cf9ee12afa3d1ff59d6b135d1fe8e563a41f2635
Mati <ratajczak.mat@gmail.com>
2025-02-03 11:54:03 +0100

    Fix feedback view for not logged users

--- backend/reelminder/portal/views.py
+++ backend/reelminder/portal/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Feedback
 from .serializers import FeedbackSerializer
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 
-@permission_classes([AllowAny])
 class FeedbackView(generics.CreateAPIView):
     """
     Endpoint responsible for creating feedback records.
 
     """
-
+    authentication_classes = []
+    permission_classes = [AllowAny]
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
